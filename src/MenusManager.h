ddb34d690fc048c057c636da109404f76fc31754
Margarida Viterbo <margaridaviterbo@hotmail.com>
2017-03-23 18:25:12 +0000

    changed to windows

---
+++ src/MenusManager.h
@@ -0,0 +1,13 @@
+#ifndef MENUSMANAGER
+#define MENUSMANAGER
+
+
+#include <iostream>
+
+using namespace std;
+
+void displayMainMenu(){
+	cout << "Windows!";
+}
+
+#endif MENUSMANAGER	/* MENUSMANAGER */
---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+#include "MenusManager.h"
+using namespace std;
+
+
+int main(){
+	cout << "Welcome to this BikeSharing Platform!";
+	displayMainMenu();
+
+	return 0;
+}
