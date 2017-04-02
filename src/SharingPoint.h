82275ab90601483e6837b194f69ee290b50d0d88
Margarida Viterbo <margaridaviterbo@hotmail.com>
2017-04-02 23:22:37 +0100

    class SharingPoint done

--- src/SharingPoint.h
+++ src/SharingPoint.h
@@ -3,14 +3,23 @@
 
 
 #include <iostream>
+#include <cstdlib>
 
 using namespace std;
 
 class SharingPoint{
 private:
-
+	int numFreeSpots;
+	bool heightDiscount = false;
+	bool suburbsDiscount = false;
 public:
-
+	SharingPoint();
+	int getNumFreeSpots();
+	bool getHeightDiscount();
+	bool getSuburbsDiscount();
+	void updateFreeSpots();
+	void setHeightDiscount();
+	void setSuburbsDiscount();
 };
 
 
